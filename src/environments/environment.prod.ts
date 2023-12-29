99935dfa07dc383869377937e0287a41ba8f7626
Kiridepapel <monalek0622@hotmail.com>
2023-12-29 12:15:57 -0500

    feat(dev): test 10 environments on vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@
 export const environment = {
   production: true,
   env: 'production',
-  GITHUB_CLIENT_ID: '${**process.env.GITHUB_CLIENT_ID**}',
-  GITHUB_CLIENT_SECRET: '${**process.env.GITHUB_CLIENT_SECRET**}',
+  GITHUB_CLIENT_ID: '60ff77068ac76e43c8a3',
+  GITHUB_CLIENT_SECRET: '92cec680fee19c7577b6ea6b02a5cce0b8078238',
 };
