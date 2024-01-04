e2d608a33cd84cc171b2b7fafc72b2915524dd17
Kiridepapel <monalek0622@hotmail.com>
2024-01-03 21:48:46 -0500

    feat(about-me): about-me section completed

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,7 +6,20 @@ import { ButtonComponent } from '../button/button.component';
   standalone: true,
   imports: [ButtonComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  public download() {
+    const pdfUrl = 'assets/pdfs/CV_Brian_Uceda.pdf';
+    const link = document.createElement('a');
+
+    link.href = pdfUrl;
+    link.target = '_blank';
+    link.download = 'CV_Brian_Uceda.pdf';
+
+    // Simular un clic en el enlace para iniciar la descarga
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
 }
